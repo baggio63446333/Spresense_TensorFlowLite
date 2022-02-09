981618b1f96ce1fe362e17f37c1a097b2e7fea4a
baggio63446333 <baggio63446333@users.noreply.github.com>
2022-02-09 23:28:10 +0900

    Set the baseline for Spresense
    Update library properties to avoid conflict with the original one.
    - Library name: Arduino_TensorFlowLite to Spresense_TensorFlowLite
    - Header name: TensorFlowLite.h to TensorFlowLite_Spresense.h

--- examples/person_detection/person_detection.ino
+++ examples/person_detection/person_detection.ino
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include <TensorFlowLite.h>
+#include <TensorFlowLite_Spresense.h>
 
 #include "main_functions.h"
 
